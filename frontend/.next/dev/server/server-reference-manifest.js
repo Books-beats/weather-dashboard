97052488f6300aef2c6cd74599e01e6cdd48cf70
Riya <riyakumari.nida@gmail.com>
2025-12-07 12:30:22 +0530

    remove .next folder

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LwK93J9OoWJxVUhpBhU+U2U6r20bZsMMkTd111LD9YQ=\"\n}"
--- frontend/.next/dev/static/chunks/591d0_next_error_3ffe8475.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/projects/weather-dashboard/frontend/node_modules/next/error.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/projects/weather-dashboard/frontend/node_modules/next/dist/pages/_error.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=591d0_next_error_3ffe8475.js.map
